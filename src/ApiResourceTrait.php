9430e5e4e1b4761586087f27399580bd212bc266
Eric Jose Cordova Maiz <48815726+Ericcor@users.noreply.github.com>
2025-02-12 16:28:21 -0400

    Update ApiResourceTrait.php

--- src/ApiResourceTrait.php
+++ src/ApiResourceTrait.php
@@ -4,108 +4,204 @@ namespace ApiMaker;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 use Illuminate\Support\Facades\DB;
 
 trait ApiResourceTrait
 {
     protected function list(string $class, Request $request)
     {
+        // New Model instance
         $model = new $class;
+
+        // Get model's listable
         $listable = $model->getListable();
+
+        // Get model's listable
         $table = $model->getTable();
 
-        // Seleccionar solo columnas necesarias
+        // Get columns to select
         $query = $model->select($model->getSelectable());
 
-        // Aplicar filtros
+        // Filters - Set wheres
         foreach ($listable as $key => $alias) {
             list($name, $column) = $this->getNameAndColumn($key, $alias, $table);
 
-            if ($request->filled($name)) {
-                $value = $request->input($name);
-                is_array($value) ? $query->whereIn($column, $value) : $query->where($column, $value);
-            } elseif ($request->filled("{$name}_not")) {
-                $query->where($column, '<>', $request->input("{$name}_not"));
-            } elseif ($request->filled("{$name}_like")) {
-                $query->where($column, 'ILIKE', '%' . $request->input("{$name}_like") . '%');
-            } elseif ($request->filled("{$name}_gte")) {
-                $query->where($column, '>=', $request->input("{$name}_gte"));
-            } elseif ($request->filled("{$name}_lte")) {
-                $query->where($column, '<=', $request->input("{$name}_lte"));
-            } elseif ($request->filled("{$name}_between")) {
-                $values = explode(',', $request->input("{$name}_between"));
-                if (count($values) === 2) {
-                    $query->whereBetween($column, $values);
+            // Where equals to
+            if ($request->has($name)) {
+                if (is_array($value = $request->get($name)) || count($valueArray = explode(',', $value)) > 1) {
+                    $query->whereIn($column, $valueArray ?? $value);
+                    continue;
                 }
-            } elseif ($request->filled("{$name}_null")) {
-                $query->whereNull($column);
-            } elseif ($request->filled("{$name}_not_null")) {
+
+                $query->where($column, $value);
+                
+            // Where not equals to
+            } elseif ($request->has($name . '_not')) {
+                if (is_array($value = $request->get($name . '_not')) || count($valueArray = explode(',', $value)) > 1) {
+                    $query->whereNotIn($column, $valueArray ?? $value);
+                    continue;
+                }
+
+                $query->where($column, '<>', $value);
+                
+            // Where like to
+            } elseif ($request->has($name . '_like')) {
+                $tablePrefix = env('DB_PREFIX');
+                $query->whereRaw($this->getLikeOperator($tablePrefix, $column, $request->get($name . '_like')));
+            } elseif ($request->has($name . '_gte') || $request->has($name . '_lte')) {
+
+                // Where greater than or equals to
+                if ($request->has($name . '_gte')) {
+                    $value = $request->get($name . '_gte');
+
+                    $query->where($column, '>=', $value);
+                }
+
+                // Where less than or equals to
+                if ($request->has($name . '_lte')) {
+                    $value = $request->get($name . '_lte');
+
+                    $query->where($column, '<=', $value);
+                }
+            // Where between
+            } elseif ($request->has($name . '_between')) {
+                $value = $request->get($name . '_between');
+
+                $valueArray = !is_array($value) ? explode(',', $value) : $value;
+
+                $query->whereBetween($column, $valueArray);
+            // Where null
+            } elseif ($request->has($name . '_null')) {
+                $value = $request->get($name . '_null');
+                if (is_null($value)) {
+                    $query->whereNull($column);
+                } elseif ($value) {
+                    $query->whereNotNull($column);
+                }
+            // Where not null
+            } elseif ($request->has($name . '_not_null')) {
                 $query->whereNotNull($column);
             }
         }
 
-        // Aplicar filtros por relaciones solo si se requieren
-        if ($request->filled('has')) {
-            $hasRelations = json_decode($request->input('has'), true);
-            if (is_array($hasRelations)) {
-                foreach ($hasRelations as $name => $values) {
-                    $relations = explode('.', $name);
-                    $column = array_pop($relations);
-
-                    $query->whereHas(array_shift($relations), function ($q) use ($relations, $column, $values) {
-                        foreach ($relations as $relation) {
-                            $q->whereHas($relation, fn($q) => $q->whereIn("{$q->getModel()->getTable()}.{$column}", $values));
-                        }
-                    });
+        // Filters by relations
+        if ($request->has('has')) {
+            if ($has = json_decode($request->get('has'), true)) {
+                foreach ($has as $name => $values) {
+                    list($relation, $column) = explode('.', $name);
+                        $query->whereHas($relation, function ($query) use ($column, $values) {
+                            $query->whereIn("{$query->getModel()->getTable()}.{$column}", $values);
+                        });
                 }
             }
         }
 
-        // Filtros con LIKE en relaciones
-        if ($request->filled('has_like')) {
-            $hasLike = json_decode($request->input('has_like'), true);
-            foreach ($hasLike as $name => $values) {
-                $relations = explode('.', $name);
-                $column = array_pop($relations);
-                $mainRelation = array_shift($relations);
+        // Set order by
+        if ($request->has('order_by')) {
+            $orderByArray = explode(',', $request->get('order_by'));
 
-                $query->whereHas($mainRelation, function ($q) use ($relations, $column, $values) {
-                    foreach ($relations as $relation) {
-                        $q = $q->whereHas($relation);
-                    }
-                    $q->where(fn($q) => array_walk($values, fn($value) => $q->orWhere($column, 'ILIKE', "%{$value}%")));
-                });
-            }
-        }
-
-        // Ordenamiento con validación
-        if ($request->filled('order_by')) {
-            foreach (explode(',', $request->input('order_by')) as $order) {
-                $orderData = $this->getOrderBy($order);
-                if ($orderData && $this->validateOrderBy($orderData, $model, $listable)) {
-                    $query = $this->applyOrderBy($query, $orderData);
+            foreach ($orderByArray as $orderByRaw) {
+                if (
+                   ($orderBy = $this->getOrderBy($orderByRaw, $listable)) !== false
+                    &&
+                    $this->validateOrderBy($orderBy, $model, $listable)
+                ) {
+                    $query = $this->applyOrderBy($query, $orderBy);
                 }
             }
         }
 
-        // Cargar relaciones solo si se solicita
-        if ($request->filled('with')) {
-            $query->with($this->getEagerLoadedRelations($request, $class));
-        }
+        // Load relations
+        $query->with($this->getEagerLoadedRelations($request, $class));
 
-        // Permitir modificar la consulta antes de ejecutarla
+        // Alter the query before getting the items
         if (method_exists($this, 'alterQuery')) {
             $query = $this->alterQuery($query);
         }
 
-        // Manejo de paginación optimizado
-        $perPage = (int) $request->input('per_page', $this->perPage ?? 20);
+        $perPage = $request->get('per_page') ?? $this->perPage ?? 20;
 
         if ($perPage == 0) {
-            return $query->get();
+            return $this->noPagination($request, $query);
+        } elseif ($request->has('paginate')) {
+            switch ($request->get('paginate')) {
+                case 'normal':
+                    return $this->normalPagination($request, $query, $perPage, $listable);
+                    break;
+
+                case 'simple':
+                    return $this->simplePagination($request, $query, $perPage);
+                    break;
+
+                case 'none':
+                    return $this->noPagination($request, $query);
+                    break;
+                
+                default:
+                    return $this->normalPagination($request, $query, $perPage, $listable);
+                    break;
+            }
         }
 
-        return $query->paginate($perPage);
+        return $this->normalPagination($request, $query, $perPage, $listable);
+    }
+
+    protected function noPagination($request, $query) {
+        $items = $query
+            ->get()
+        ;
+
+        $items->each(function ($resource) use ($request) {
+                $resource->append($this->getAppendableAttributes($request));
+            })
+        ;
+
+        return collect(['data' => $items]);
+    }
+
+    protected function normalPagination($request, $query, $perPage, $listable) {
+        
+        $query_string = array_merge($listable, ['order_by', 'per_page', 'with']);
+
+        $paginated = $query
+            ->paginate($perPage)
+            ->appends($request->only($query_string))
+        ;
+
+        $items = collect($paginated->items())
+            ->each(function ($resource) use ($request) {
+                $resource->append($this->getAppendableAttributes($request));
+            })
+        ;
+
+        return $paginated;
+    }
+
+    protected function simplePagination($request, $query, $perPage) {
+        $paginated = $query
+            ->simplePaginate($perPage)
+        ;
+
+        $items = collect($paginated->items())
+            ->each(function ($resource) use ($request) {
+                $resource->append($this->getAppendableAttributes($request));
+            })
+        ;
+
+        return $paginated;
+    }
+
+    protected function getLikeOperator($tablePrefix, $column, $value) {
+        return "UPPER({$tablePrefix}{$column}) LIKE UPPER('%{$value}%')";
+    }
+
+    private function getAppendableAttributes(Request $request)
+    {
+        if (!$request->has('append')) {
+            return [];
+        }
+        return explode(',', $request->get('append'));
     }
 
     private function getNameAndColumn($key, $alias, $table)
@@ -127,25 +223,29 @@ trait ApiResourceTrait
         ];
     }
 
-    protected function getOrderBy($orderBy)
+    private function getOrderBy($orderBy)
     {
         $array = explode(':', $orderBy);
-        return isset($array[0]) ? (object) [
+
+        return (object) [
             'column' => $array[0],
-            'order' => isset($array[1]) && in_array(strtoupper($array[1]), ['ASC', 'DESC']) ? $array[1] : 'ASC',
-        ] : false;
+            'order' => $array[1] ?? 'ASC',
+        ];
     }
 
-    private function validateOrderBy($orderBy, $model, array $columns = [])
+    private function validateOrderBy($order_by, $model, array $columns = [])
     {
-        return in_array($orderBy->column, $columns) && in_array(strtoupper($orderBy->order), ['ASC', 'DESC']);
-    }
+        if (empty($columns) || !in_array(strtoupper($order_by->order), ['ASC', 'DESC'])) {
+            return false;
+        }
 
-    private function getEagerLoadedRelations(Request $request, $model)
-    {
-        return collect(explode(',', $request->get('with', '')))->filter(
-            fn($relation) => method_exists($model, $relation)
-        )->toArray();
+        if (in_array($order_by->column, $columns)) {
+            return true;
+        } elseif (count($name_array = explode('.', $order_by->column)) == 2) {
+            return method_exists($model, $name_array[0]);
+        }
+
+        return false;
     }
 
     private function applyOrderBy($query, $order_by)
@@ -170,5 +270,59 @@ trait ApiResourceTrait
         }
 
         return $query->orderBy($order_by->column, $order_by->order);
+    }
+
+    private function getEagerLoadedRelations(Request $request, $model)
+    {
+        return collect(explode(',', $request->get('with')))->filter(
+            function ($relation) use ($model) {
+                $relationArray = explode('.', $relation);
+                return method_exists($model, current($relationArray));
+            }
+        )->toArray();
+    }
+
+    protected function find(string $class, $id, $pk = null)
+    {
+        $model = (new $class);
+
+        if (!is_null($pk)) {
+            $model->setKeyName($pk);
+        }
+
+        return $model->select($model->getSelectable())
+            ->with($this->getEagerLoadedRelations(request(), $class))
+            ->findOrFail($id)
+            ->append($this->getAppendableAttributes(request()))
+        ;
+    }
+
+    protected function new(string $class, Request $request)
+    {
+        $resource = new $class;
+
+        $input = $request->only($resource->getFillable());
+
+        $resource->fill($this->alterInput($input));
+
+        $resource->save();
+
+        return $resource->refresh();
+    }
+
+    protected function alterInput(array $input): array
+    {
+        return $input;
+    }
+
+    protected function edit(Model $resource, Request $request)
+    {
+        $input = $request->only($resource->getFillable());
+
+        $resource->fill($this->alterInput($input));
+
+        $resource->save();
+
+        return $resource->fresh();
     }
 }
